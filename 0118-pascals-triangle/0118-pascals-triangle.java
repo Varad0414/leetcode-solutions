857d9f7902207f433729963b72dee28ee47f4e96
Varad Uplanchiwar <100687358+Varad0414@users.noreply.github.com>
2023-09-08 12:42:59 +0530

    Time: 0 ms (100%), Space: 41.2 MB (38.24%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,46 +1,23 @@
 class Solution {
+    public static ArrayList<Integer> getRow(int row){
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(1);
+        int num = 1;
+        for(int i=1;i<row;i++){
+            num = num * (row - i);
+            num = num / i;
+            al.add(num); 
+        }
+
+        return al;
+    }
+
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> al = new ArrayList<>();
-
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        al.add(list);
-        if(numRows == 1){
-            return al;
+        for(int i=1;i<=numRows;i++){
+            al.add(getRow(i));
         }
 
-        List<Integer> list2 = new ArrayList<>();
-        list2.add(1);
-        list2.add(1);
-        al.add(list2);
-        if(numRows == 2){
-            return al;
-        }
-
-        for(int i=2;i<numRows;i++){
-            List<Integer> list3 = new ArrayList<>();
-
-            for(int j=0;j<i+1;j++){
-                int n1 = 0;
-                if((j-1) < 0){
-                    n1 = 0;
-                }
-                else{
-                    n1 = al.get(i-1).get(j-1);
-                }
-                int n2 = 0;
-                if(j >= al.get(i-1).size()){
-                    n2 = 0;
-                }
-                else{
-                    n2 = al.get(i-1).get(j);
-                }
-                int sum = n1 + n2;
-                list3.add(sum);
-            }
-
-            al.add(list3);
-        }
         return al;
     }
 }
