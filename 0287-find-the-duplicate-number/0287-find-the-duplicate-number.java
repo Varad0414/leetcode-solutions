f4f94cefcf24bd618dbc389a958fcf9522d72e71
Varad Uplanchiwar <100687358+Varad0414@users.noreply.github.com>
2023-09-19 14:08:15 +0530

    Time: 35 ms (18.1%), Space: 56.1 MB (91.7%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,47 +1,12 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        // int n = nums.length;
-        // int esum = (n*(n+1))/2;
-        // int sum = 0;
-        // int dupli = nums[0];
-        // int cnt = 0;
-        // for(int i=0;i<n;i++){
-        //     sum += nums[i];
-        //     if(dupli == nums[i]){
-        //         cnt++;
-        //     } 
-        // }
-
-        // if(cnt >= 2)
-        //     return dupli;
-        // // if(esum == sum)
-        // //     return nums[0];
-        // dupli = (n - (esum - sum));
-        // cnt = 0;
-        // for(int i=0;i<n;i++){
-        //     if(dupli == nums[i]){
-        //         cnt++;
-        //     }
-        // }
-
-        // if(cnt > 1){
-        //     return dupli;
-        // }
-        // else{
-
-        // }
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int n = 0;
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i], map.getOrDefault(nums[i],0) + 1);
-            if(map.get(nums[i]) >= 2){
-                n = nums[i];
+        Arrays.sort(nums);
+        int i = 0;
+        for(i=0;i<nums.length - 1;i++){
+            if(nums[i] == nums[i+1]){
                 break;
             }
         }
-
-        return n;
-
+        return nums[i];
     }
 }
